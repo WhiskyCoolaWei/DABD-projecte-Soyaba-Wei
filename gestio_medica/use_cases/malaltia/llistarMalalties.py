75c29a11e984e62b00ada7972d90d15bc1c40699
Wei <mrweiz03@gmail.com>
2026-06-10 13:40:18 +0200

    Revert "Feature/login malaltia"

--- gestio_medica/use_cases/malaltia/llistarMalalties.py
+++
@@ -1,9 +0,0 @@
-from gestio_medica.models import Malaltia
-
-
-def llistarMalalties(cerca: str = ''):
-    qs = Malaltia.objects.all().order_by('nom')
-    if cerca:
-        qs = qs.filter(nom__icontains=cerca) | \
-             Malaltia.objects.filter(codi_cie__icontains=cerca)
-    return list(qs)
